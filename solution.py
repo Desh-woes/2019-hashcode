d234c70cd639a89ab118773efd91641a55a098a2
Olivier-rw <onshimiyimana17@alustudent.com>
2019-02-28 21:19:16 +0200

    Improved solution

--- solution.py
+++ solution.py
@@ -1,27 +1,29 @@
 from object_file import Image, Slide
-filename = 'e_shiny_selfies.txt'
+filename = 'd_pet_pictures.txt'
 f = open('files/'+filename, 'r')
 
 total_photos = int(f.readline())
 
 # print(total_photos)
-horizontal_images_arr = []
-vertical_images_arr = []
-
+horizontal_images_arr = {}
+vertical_images_arr = {}
+list_index = 0
 for i in range(total_photos):
     read_list = f.readline().split()
     orientation = read_list[0]
     tags = read_list[2:]
     new_object = Image(orientation, tags)
     if orientation == 'H':
-        horizontal_images_arr.append(new_object)
+        horizontal_images_arr[list_index] = new_object
     else:
-        vertical_images_arr.append(new_object)
+        vertical_images_arr[list_index] = new_object
+    list_index += 1
 
 # for image in images_arr:
 #     print(image)
 n_horizontal = len(horizontal_images_arr)
 n_vertical = len(vertical_images_arr)
+
 
 # for image in images_arr:
 #     if image.orientation == 'H':
@@ -39,16 +41,16 @@ new_slide = Slide(total_slides)
 count = 0
 while count < total_slides:
     for y in horizontal_images_arr:
-        if not y.used:
-            new_slide.slide_desc.append(horizontal_images_arr.index(y))
-            y.used = True
+        if not horizontal_images_arr[y].used:
+            new_slide.slide_desc.append(y)
+            horizontal_images_arr[y].used = True
             count += 1
 
     new_arr = []
     for y in vertical_images_arr:
-        if not y.used and len(new_arr) < 2:
-            new_arr.append(vertical_images_arr.index(y)+ len(horizontal_images_arr))
-            y.used = True
+        if not vertical_images_arr[y].used and len(new_arr) < 2:
+            new_arr.append(y)
+            vertical_images_arr[y].used = True
     new_slide.slide_desc.append(new_arr)
     count += 1
 
