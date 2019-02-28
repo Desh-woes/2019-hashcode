2e0951bb854f663946a5f3fc219ca5da6b1e40e7
Olivier-rw <onshimiyimana17@alustudent.com>
2019-02-28 20:07:33 +0200

    read input

---
+++ object_file.py
@@ -0,0 +1,9 @@
+class Image:
+    def __init__(self, orientation, tag_arr):
+        self.orientation = orientation
+        self.tag_arr = tag_arr
+        self.n_tags = len(tag_arr)
+        self.used = False
+
+    def __str__(self):
+        return self.orientation + ' ' + str(self.n_tags) + ' ' + ' '.join(i for i in self.tag_arr)
--- solution.py
+++ solution.py
@@ -1,1 +1,18 @@
+from object_file import Image
 f = open('files/a_example.txt', 'r')
+
+total_photos = int(f.readline())
+
+# print(total_photos)
+images_arr = []
+
+for i in range(total_photos):
+    read_list = f.readline().split()
+    orientation = read_list[0]
+    tags = read_list[2:]
+    new_object = Image(orientation, tags)
+    images_arr.append(new_object)
+
+# for image in images_arr:
+#     print(image)
+
