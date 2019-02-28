9dcce26b8504dc56d5be5d0b3710e8cc84af51f8
Olivier-rw <onshimiyimana17@alustudent.com>
2019-02-28 21:02:52 +0200

    Initial solution

--- solution.py
+++ solution.py
@@ -1,47 +1,54 @@
 from object_file import Image, Slide
-f = open('files/e_shiny_selfies.txt', 'r')
+filename = 'e_shiny_selfies.txt'
+f = open('files/'+filename, 'r')
 
 total_photos = int(f.readline())
 
 # print(total_photos)
-images_arr = []
+horizontal_images_arr = []
+vertical_images_arr = []
 
 for i in range(total_photos):
     read_list = f.readline().split()
     orientation = read_list[0]
     tags = read_list[2:]
     new_object = Image(orientation, tags)
-    images_arr.append(new_object)
+    if orientation == 'H':
+        horizontal_images_arr.append(new_object)
+    else:
+        vertical_images_arr.append(new_object)
 
 # for image in images_arr:
 #     print(image)
-n_horizontal = 0
-n_vertical = 0
-for image in images_arr:
-    if image.orientation == 'H':
-        n_horizontal += 1
-    elif image.orientation == 'V':
-        n_vertical += 1
+n_horizontal = len(horizontal_images_arr)
+n_vertical = len(vertical_images_arr)
+
+# for image in images_arr:
+#     if image.orientation == 'H':
+#         n_horizontal += 1
+#     elif image.orientation == 'V':
+#         n_vertical += 1
 
 total_slides = n_horizontal + n_vertical//2
 print(total_slides)
 
-output = open('output', 'a')
+output = open(filename + 'output', 'a')
 output.write(str(total_slides) + '\n')
 
 new_slide = Slide(total_slides)
 count = 0
 while count < total_slides:
-    for y in images_arr:
-        if not y.used and y.orientation == "H":
-            new_slide.slide_desc.append(images_arr.index(y))
+    for y in horizontal_images_arr:
+        if not y.used:
+            new_slide.slide_desc.append(horizontal_images_arr.index(y))
             y.used = True
             count += 1
 
     new_arr = []
-    for y in images_arr:
-        if not y.used and y.orientation == "V" and len(new_arr) < 2:
-            new_arr.append(images_arr.index(y))
+    for y in vertical_images_arr:
+        if not y.used and len(new_arr) < 2:
+            new_arr.append(vertical_images_arr.index(y)+ len(horizontal_images_arr))
+            y.used = True
     new_slide.slide_desc.append(new_arr)
     count += 1
 
